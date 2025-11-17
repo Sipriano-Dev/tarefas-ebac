6475122e55ce7637d281e26df61dd8bd6d4ae7dd
Anderson Sipriano <adsspns@gmail.com>
2025-11-17 18:36:22 -0300

    Tarefa Mod 18

---
+++ mod18/src/com/sipriano/carros/Carro.java
@@ -0,0 +1,23 @@
+package com.sipriano.carros;
+
+import com.sipriano.annotation.Tabela;
+
+@Tabela("carros")
+public class Carro {
+    private String modelo;
+    private int ano;
+
+    public Carro(String modelo, int ano) {
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+}
+

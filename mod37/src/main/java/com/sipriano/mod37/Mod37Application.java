f19f07d190471776667b083cbfc064c3184b1ed0
Anderson Sipriano <adsspns@gmail.com>
2025-12-19 16:45:49 -0300

    Tarefa Mod 37

---
+++ mod37/src/main/java/com/sipriano/mod37/Mod37Application.java
@@ -0,0 +1,13 @@
+package com.sipriano.mod37;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mod37Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mod37Application.class, args);
+	}
+
+}
